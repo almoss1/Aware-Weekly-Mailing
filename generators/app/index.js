9f328f5fb4dd2c7e3996092452eee0bf17f61d07
Alex Mosseri <aljmo98@gmail.com>
2019-07-02 17:13:11 -0400

    updated

--- generators/app/index.js
+++ generators/app/index.js
@@ -50,8 +50,8 @@ module.exports = class extends Generator {
 
   writing() {
     this.fs.copy(
-      this.templatePath('./server/*'),
-      this.destinationPath('generators/templates/server/'));
+      this.templatePath('../../*'),
+      this.destinationPath('generators/'));
 
     // fse.copySync('_package.json', 'package.json')
     // fse.copySync('_README.md', 'README.md')
