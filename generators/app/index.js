139f672cffb46900a1c978b697818cd4c5a1ecde
Alex Mosseri <aljmo98@gmail.com>
2019-07-02 10:03:52 -0400

    updated

--- generators/app/index.js
+++ generators/app/index.js
@@ -6,7 +6,7 @@ var chalk = require('chalk');
 var yosay = require('yosay');
 
 
-module.exports = yeoman.generators.Base.extend({
+module.exports = yeoman.extend({
 //Configurations will be loaded here.
 
     prompting: function() {
