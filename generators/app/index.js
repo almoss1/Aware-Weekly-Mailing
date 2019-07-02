e9f16703f4d11d0b47fab675f20711b72361969d
Alex Mosseri <aljmo98@gmail.com>
2019-07-02 11:51:51 -0400

    updated

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,9 +1,265 @@
 
 'use strict';
 //Require dependencies
-var yeoman = require('yeoman-generator');
+var Generator = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
+
+
+
+module.exports = class extends Generator {
+
+  async prompting() {
+
+    // Have Yeoman greet the user.
+
+    this.log(
+
+      yosay(`Welcome to the spectacular ${chalk.red('generator-aware-weekly-mailing-g')} generator!`)
+
+    );
+
+    const prompts = [
+
+      {
+
+        type: 'confirm',
+
+        name: 'someAnswer',
+
+        message: 'Would you like to enable this option?',
+
+        default: true
+
+      }
+
+    ];
+
+    const props = await this.prompt(prompts);
+
+    // To access props later use this.props.someAnswer;
+
+    this.props = props;
+
+  }
+
+  writing() {
+      this.fs.copyTpl(
+        this.templatePath('_package.json'),
+        this.destinationPath('package.json'), {
+            name: this.props.name
+          }
+      );
+      this.fs.copyTpl(
+        this.templatePath('_README.md'),
+        this.destinationPath('README.md'), {
+            name: this.props.name
+          }
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('_generators/templates/server/_README.md'),
+        this.destinationPath('generators/templates/server/README.md'), {
+            name: this.props.name
+          }
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('_generators/templates/server/_package.json'),
+        this.destinationPath('generators/templates/server/package.json'), {
+            name: this.props.name
+          }
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('_generators/templates/server/_modernizr.json'),
+        this.destinationPath('generators/templates/server/modernizr.json'), {
+            name: this.props.name
+          }
+      );
+
+      this.fs.copyTpl(
+        this.templatePath('_generators/templates/server/_ldws-server.code-workspace'),
+        this.destinationPath('generators/templates/server/ldws-server.code-workspace'), {
+            name: this.props.name
+          }
+      );
+
+      
+  
+
+  //Copy application files
+
+  /////Generators
+  /////APP
+  
+this.fs.copy(
+  this.templatePath('_app/templates/_dummyfile.txt'),
+  this.destinationPath('app/templates/dummyfile.txt'));
+      
+this.fs.copy(
+  this.templatePath('_app/templates/_dummyfile.txt'),
+  this.destinationPath('app/index.js'));
+      
+
+  ///templates
+  //server
+    ///////data
+    ///////cache
+
+     /////alerts
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/cache/_alerts.json'),
+    this.destinationPath('generators/templates/server/data/cache/alerts.json'));
+  ////solutions
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/cache/_solutions.json'),
+    this.destinationPath('generators/templates/server/data/cache/alerts.json'));
+    ////status
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/cache/_status.json'),
+    this.destinationPath('generators/templates/server/data/cache/alerts.json'));
+  ///config
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/config/_database.txt'),
+    this.destinationPath('generators/templates/server/data/config/database.txt'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/config/_sentry.txt'),
+    this.destinationPath('generators/templates/server/data/config/sentry.txt'));
+
+
+  //////sql
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/config/sql/_alerts.sql'),
+    this.destinationPath('generators/templates/server/data/config/sql/alerts.sql'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/config/sql/_minmax.sql'),
+    this.destinationPath('generators/templates/server/data/config/sql/minmax.sql'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/config/sql/_solutions.sql'),
+    this.destinationPath('generators/templates/server/data/config/sql/solutions.sql'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/config/sql/_status.sql'),
+    this.destinationPath('generators/templates/server/data/config/sql/status.sql'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/config/sql/_wetndry.sql'),
+    this.destinationPath('generators/templates/server/data/config/sql/wetndry.sql'));
+    
+        /////logs
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/logs'),
+    this.destinationPath('generators/templates/server/data/logs'));
+
+    ///.gitkeep
+
+              ///////secrets
+
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/secrets/_aware-storage-account.txt'),
+    this.destinationPath('generators/templates/server/data/secrets/aware-storage-account.txt'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/secrets/_dababase-aware.txt'),
+    this.destinationPath('generators/templates/server/data/secrets/dababase-aware.txt'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/secrets/_dababase-dev.txt'),
+    this.destinationPath('generators/templates/server/data/secrets/dababase-dev.txt'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/secrets/_dababase-prod.txt'),
+    this.destinationPath('generators/templates/server/data/secrets/dababase-prod.txt'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/data/secrets/_sentry-test.txt'),
+    this.destinationPath('generators/templates/server/data/secrets/sentry-test.txt'));
+
+
+          ///////src
+
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/_config.js'),
+    this.destinationPath('generators/templates/server/src/config.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/_list.js'),
+    this.destinationPath('generators/templates/server/src/list.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/_refresh.js'),
+    this.destinationPath('generators/templates/server/src/refresh.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/_refresh.test.js'),
+    this.destinationPath('generators/templates/server/src/config.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/_server.js'),
+    this.destinationPath('generators/templates/server/src/server.js'));
+              //////config
+  
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_database.js'),
+    this.destinationPath('generators/templates/server/src/config/database.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_database.test.js'),
+    this.destinationPath('generators/templates/server/src/config/database.test.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_locations.js'),
+    this.destinationPath('generators/templates/server/src/config/locations.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_locations.test.js'),
+    this.destinationPath('generators/templates/server/src/config/locations.test.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_logging.js'),
+    this.destinationPath('generators/templates/server/src/config/logging.js'));
+
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_logging.test.js'),
+    this.destinationPath('generators/templates/server/src/config/logging.test.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_tracker.js'),
+    this.destinationPath('generators/templates/server/src/config/tracker.js'));
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/src/config/_tracker.test.js'),
+    this.destinationPath('generators/templates/server/src/config/tracker.js'));
+
+  this.fs.copy(
+    this.templatePath('_generators/templates/server/_gulpfile.js'),
+    this.destinationPath('generators/templates/server/src/config/gulpfile.js'));
+
+
+
+
+  this.fs.copy(
+    this.templatePath('_.editorconfig'),
+    this.destinationPath('editorconfig'));
+
+  this.fs.copy(
+    this.templatePath('_.eslintignore'),
+    this.destinationPath('.eslintignore'));
+
+ 
+  this.fs.copy(
+    this.templatePath('_.gitattributes'),
+    this.destinationPath('.gitattributes'));
+
+
+  this.fs.copy(
+    this.templatePath('_.gitignore'),
+    this.destinationPath('.gitignore'));
+
+  this.fs.copy(
+    this.templatePath('_.travis.yml'),
+    this.destinationPath('.travis.yml'));
+
+  this.fs.copy(
+    this.templatePath('_.yo-rc.json'),
+    this.destinationPath('.yo-rc.json'));
+
+  }
+
+  install() {
+
+    this.installDependencies();
+
+  }
+
+
+};
+
 
 
 // module.exports = yeoman.generator.Base.extend({
@@ -248,262 +504,6 @@ var yosay = require('yosay');
     
 //     });
 
-
-
-
-
-    module.exports = class extends Generator {
-
-        async prompting() {
-      
-          // Have Yeoman greet the user.
-      
-          this.log(
-      
-            yosay(`Welcome to the spectacular ${chalk.red('generator-aware-weekly-mailing-g')} generator!`)
-      
-          );
-      
-          const prompts = [
-      
-            {
-      
-              type: 'confirm',
-      
-              name: 'someAnswer',
-      
-              message: 'Would you like to enable this option?',
-      
-              default: true
-      
-            }
-      
-          ];
-      
-          const props = await this.prompt(prompts);
-      
-          // To access props later use this.props.someAnswer;
-      
-          this.props = props;
-      
-        }
-      
-        writing() {
-            this.fs.copyTpl(
-              this.templatePath('_package.json'),
-              this.destinationPath('package.json'), {
-                  name: this.props.name
-                }
-            );
-            this.fs.copyTpl(
-              this.templatePath('_README.md'),
-              this.destinationPath('README.md'), {
-                  name: this.props.name
-                }
-            );
-
-            this.fs.copyTpl(
-              this.templatePath('_generators/templates/server/_README.md'),
-              this.destinationPath('generators/templates/server/README.md'), {
-                  name: this.props.name
-                }
-            );
-
-            this.fs.copyTpl(
-              this.templatePath('_generators/templates/server/_package.json'),
-              this.destinationPath('generators/templates/server/package.json'), {
-                  name: this.props.name
-                }
-            );
-
-            this.fs.copyTpl(
-              this.templatePath('_generators/templates/server/_modernizr.json'),
-              this.destinationPath('generators/templates/server/modernizr.json'), {
-                  name: this.props.name
-                }
-            );
-
-            this.fs.copyTpl(
-              this.templatePath('_generators/templates/server/_ldws-server.code-workspace'),
-              this.destinationPath('generators/templates/server/ldws-server.code-workspace'), {
-                  name: this.props.name
-                }
-            );
-
-            
-        
-
-        //Copy application files
-   
-        /////Generators
-        /////APP
-        
-      this.fs.copy(
-        this.templatePath('_app/templates/_dummyfile.txt'),
-        this.destinationPath('app/templates/dummyfile.txt'));
-            
-      this.fs.copy(
-        this.templatePath('_app/templates/_dummyfile.txt'),
-        this.destinationPath('app/index.js'));
-            
-
-        ///templates
-        //server
-          ///////data
-          ///////cache
-
-           /////alerts
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/cache/_alerts.json'),
-          this.destinationPath('generators/templates/server/data/cache/alerts.json'));
-        ////solutions
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/cache/_solutions.json'),
-          this.destinationPath('generators/templates/server/data/cache/alerts.json'));
-          ////status
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/cache/_status.json'),
-          this.destinationPath('generators/templates/server/data/cache/alerts.json'));
-        ///config
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/config/_database.txt'),
-          this.destinationPath('generators/templates/server/data/config/database.txt'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/config/_sentry.txt'),
-          this.destinationPath('generators/templates/server/data/config/sentry.txt'));
-
-
-        //////sql
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/config/sql/_alerts.sql'),
-          this.destinationPath('generators/templates/server/data/config/sql/alerts.sql'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/config/sql/_minmax.sql'),
-          this.destinationPath('generators/templates/server/data/config/sql/minmax.sql'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/config/sql/_solutions.sql'),
-          this.destinationPath('generators/templates/server/data/config/sql/solutions.sql'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/config/sql/_status.sql'),
-          this.destinationPath('generators/templates/server/data/config/sql/status.sql'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/config/sql/_wetndry.sql'),
-          this.destinationPath('generators/templates/server/data/config/sql/wetndry.sql'));
-          
-              /////logs
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/logs'),
-          this.destinationPath('generators/templates/server/data/logs'));
-
-          ///.gitkeep
-
-                    ///////secrets
-
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/secrets/_aware-storage-account.txt'),
-          this.destinationPath('generators/templates/server/data/secrets/aware-storage-account.txt'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/secrets/_dababase-aware.txt'),
-          this.destinationPath('generators/templates/server/data/secrets/dababase-aware.txt'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/secrets/_dababase-dev.txt'),
-          this.destinationPath('generators/templates/server/data/secrets/dababase-dev.txt'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/secrets/_dababase-prod.txt'),
-          this.destinationPath('generators/templates/server/data/secrets/dababase-prod.txt'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/data/secrets/_sentry-test.txt'),
-          this.destinationPath('generators/templates/server/data/secrets/sentry-test.txt'));
-
-
-                ///////src
-
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/_config.js'),
-          this.destinationPath('generators/templates/server/src/config.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/_list.js'),
-          this.destinationPath('generators/templates/server/src/list.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/_refresh.js'),
-          this.destinationPath('generators/templates/server/src/refresh.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/_refresh.test.js'),
-          this.destinationPath('generators/templates/server/src/config.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/_server.js'),
-          this.destinationPath('generators/templates/server/src/server.js'));
-                    //////config
-        
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_database.js'),
-          this.destinationPath('generators/templates/server/src/config/database.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_database.test.js'),
-          this.destinationPath('generators/templates/server/src/config/database.test.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_locations.js'),
-          this.destinationPath('generators/templates/server/src/config/locations.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_locations.test.js'),
-          this.destinationPath('generators/templates/server/src/config/locations.test.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_logging.js'),
-          this.destinationPath('generators/templates/server/src/config/logging.js'));
-
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_logging.test.js'),
-          this.destinationPath('generators/templates/server/src/config/logging.test.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_tracker.js'),
-          this.destinationPath('generators/templates/server/src/config/tracker.js'));
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/src/config/_tracker.test.js'),
-          this.destinationPath('generators/templates/server/src/config/tracker.js'));
-
-        this.fs.copy(
-          this.templatePath('_generators/templates/server/_gulpfile.js'),
-          this.destinationPath('generators/templates/server/src/config/gulpfile.js'));
-
-
-      
-
-        this.fs.copy(
-          this.templatePath('_.editorconfig'),
-          this.destinationPath('editorconfig'));
-    
-        this.fs.copy(
-          this.templatePath('_.eslintignore'),
-          this.destinationPath('.eslintignore'));
-    
-       
-        this.fs.copy(
-          this.templatePath('_.gitattributes'),
-          this.destinationPath('.gitattributes'));
-    
-      
-        this.fs.copy(
-          this.templatePath('_.gitignore'),
-          this.destinationPath('.gitignore'));
-
-        this.fs.copy(
-          this.templatePath('_.travis.yml'),
-          this.destinationPath('.travis.yml'));
-
-        this.fs.copy(
-          this.templatePath('_.yo-rc.json'),
-          this.destinationPath('.yo-rc.json'));
-    
-        }
-
-        install() {
-      
-          this.installDependencies();
-      
-        }
-      
-
-    };
 
 
 
